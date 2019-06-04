23b17470fdb4d160b4bdd947b42ec5764922f23d
jose51197 <jose51197@hotmail.com>
2019-06-03 19:18:01 -0600

    killer

---
+++ killer.c
@@ -0,0 +1,21 @@
+#include <iostream> 
+#include <sys/ipc.h> 
+#include <sys/shm.h> 
+#include <stdio.h> 
+using namespace std; 
+
+int main() 
+{ 
+	// ftok to generate unique key 
+	key_t key = 5678;
+
+	// shmget returns an identifier in shmid 
+	int shmid1 = shmget(key,16384,0666|IPC_CREAT); 
+    int shmid2 = shmget(key+1,20,0666|IPC_CREAT); 
+    
+	// destroy the shared memory 
+	shmctl(shmid1,IPC_RMID,NULL); 
+    shmctl(shmid2,IPC_RMID,NULL); 
+    
+	return 0; 
+} 
